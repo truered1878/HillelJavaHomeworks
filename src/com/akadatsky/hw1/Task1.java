35163dd86133a0c9ea44c67ec406ad62c934c403
Andrey Kadatsky <andrey.kadatsky@gmail.com>
2021-10-01 20:52:53 +0300

    Initial commit

---
+++ src/com/akadatsky/hw1/Task1.java
@@ -0,0 +1,9 @@
+package com.akadatsky.hw1;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        System.out.println("task1");
+    }
+
+}
---
+++ src/com/akadatsky/hw1/Task2.java
@@ -0,0 +1,9 @@
+package com.akadatsky.hw1;
+
+public class Task2 {
+
+    public static void main(String[] args) {
+        System.out.println("task2");
+    }
+
+}
