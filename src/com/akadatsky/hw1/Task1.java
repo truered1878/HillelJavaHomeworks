73571be6c5a75e5aa46488ef4cb9f45108fb39f8
Andrey Kadatsky <andrey.kadatsky@gmail.com>
2021-10-01 20:55:42 +0300

    Add Task3

--- src/com/akadatsky/hw1/Task1.java
+++ src/com/akadatsky/hw1/Task1.java
@@ -2,6 +2,7 @@ package com.akadatsky.hw1;
 
 public class Task1 {
 
+    // test
     public static void main(String[] args) {
         System.out.println("task1");
     }
---
+++ src/com/akadatsky/hw1/Task3.java
@@ -0,0 +1,4 @@
+package com.akadatsky.hw1;
+
+public class Task3 {
+}
